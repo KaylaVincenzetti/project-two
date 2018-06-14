7017c5d0ba62050293e334700e418e47cceb811c
Kayla V <knvg14@gmail.com>
2018-06-14 14:36:45 -0600

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+ } else {
 
+   connection = mysql.createConnection({
 
     host:"localhost",
     port:3306,
     user:"root",
     password:"root",
     database:"products_db"
-})
+    });
+};
 
-
-connection.connect(function(err,res){
-    console.log("Connected as id: " +connection.threadId);
-    
-});
-
+connection.connect();
 module.exports = connection;
